46355d874f9e3d6720daf79172124d38d4a14496
hieunguyen1 <xuanbachrtc@gmail.com>
2021-12-19 23:40:34 +0700

    Revert "first commit"
    This reverts commit 5d1cff7f0c752573864da203ca089df28e975cf1.

--- detectImage/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-
-class Image(models.Model):
-    img = models.CharField(max_length=500, null=True, blank=True)
-    create_time = models.TimeField(auto_now=True)
